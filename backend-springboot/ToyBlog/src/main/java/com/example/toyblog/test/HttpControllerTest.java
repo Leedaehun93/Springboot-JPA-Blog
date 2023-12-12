7e02ead8259d3325737d8094d92af4ab6b9020ab
Ivan <dzdzz93@naver.com>
2023-12-13 00:20:16 +0900

    http 실습 및 lombok 사용해보기

---
+++ backend-springboot/ToyBlog/src/main/java/com/example/toyblog/test/HttpControllerTest.java
@@ -0,0 +1,100 @@
+package com.example.toyblog.test;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * ======================================
+ * FileName : HttpControllerTest
+ * Author : DH.Lee
+ * Date : 2023-12-12
+ * Note : http 요청 실습 컨트롤러
+ * 1) 웹 브라우저가 주로 GET 요청을 사용하여 서버에 데이터를 요청한다.
+ * 웹 브라우저는 주로 GET 요청을 사용하여 서버에 데이터를 요청하며,
+ * TODO: 이 때 쿼리 스트링을 사용하는 것이 가장 일반적인 방법이다.
+ * 그러나 폼을 사용한 GET 요청도 가능합니다.
+ * (1-1) get 요청 시 RequestParam 을 사용해서 Mapping 받기
+ *      @GetMapping 메소드에서 @RequestParam을 사용하여 GET 요청의 파라미터를 매핑하는 방법
+ * (1-2) get 요청 시 Member Objet 를 활용하여 Mapping 받기
+ *      스프링 프레임워크가 객체를 통해 자동으로 요청 파라미터를 바인딩하여 쉽게 사용 할 수 있음
+ * 2) post 요청 시 RequestBody 를 사용하여 파싱한다.
+ *
+ * @RequestBody는 클라이언트로부터 받은 JSON이나 텍스트 데이터를 Java 객체로 변환하는 데 사용된다.
+ * (2-1) post 요청 시 text/plain 으로 보내는 방법
+ * @RequestBody 를 사용하여 text/plain 타입의 데이터를 String 형태로 받는 방법으로 문자열 형태의 데이터를 받아 그대로 반환한다.
+ * (2-2) application/json 으로 보내는 방법
+ * @RequestBody 를 사용하여 application/json 형태의 데이터를 받아 Member 객체로 파싱한 후, 해당 객체의 필드를 사용하여 응답 문자열을 구성한다.
+ * <p>
+ * TODO: MessageConverter 기능과 역할:
+ *        스프링 프레임워크에서 클라이언트와 서버 간의 데이터 교환을 위해
+ *        HTTP 요청 및 응답 본문을 특정 형식으로 변환하는 역할을 한다.
+ *        주로 REST API에서 JSON, XML 같은 데이터 형식으로 객체를 변환하는 데 사용되고
+ *        예시로 SON 데이터를 자바 객체로 파싱해주는 역할을 한다.(변환하거나 반대로 변환하는 역할)
+ *        REST API를 구현하는 데 있어서 데이터 형식 간의 변환이 필수적인데,
+ *        이 변환 과정을 MessageConverter가 담당하여 개발자가 보다 효율적으로 API를 구현할 수 있도록 도와준다.
+ * <p>
+ * TMI
+ * 스프링 프레임워크에서 MessageConverter의 "기능"은 HTTP 요청과 응답을 특정 데이터 형식으로 변환하는 것
+ * 반면, MessageConverter의 "역할"은 스프링 MVC 아키텍처 내에서 데이터 변환을 관리하고 REST API의 효율적인 구현을 지원하는 것
+ * ======================================
+ */
+
+// 사용자가 요청 - > 응답(HTML 파일)
+// @Controller
+
+// 사용자가 요청 -> 응답(Data)
+@RestController
+public class HttpControllerTest {
+
+    private static final String TAG = "HttpControllerTest : ";
+
+    //  TODO: http://localhost:8080/http/lombok
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+//        Member m = new Member(1, "ssar", "1234", "email");
+        Member m = Member.builder().username("ssar").password("1234").email("ssar@tate.com").build();
+        System.out.println(TAG + "getter : " + m.getUsername());
+        m.setUsername("cos");
+        System.out.println(TAG + "setter : " + m.getUsername());
+        return "lombok test 완료";
+    }
+
+// TODO: 1) 웹 브라우저가 주로 GET 요청을 사용하여 서버에 데이터를 요청한다.
+//  TODO: (1-1) get 요청 시 RequestParam 을 사용해서 Mapping 받기
+    // http://localhost:8080/http/get(select)
+//    @GetMapping("/http/get")
+//    public String getTest(@RequestParam int id, @RequestParam String username) {
+//        return "get 요청 : " + id + ", " + username ;
+//    }
+
+    //   TODO: (1-2) Member Objet 를 활용하여 Mapping 받기
+    // TODO: http://localhost:8080/http/get(select)
+    @GetMapping("/http/get")
+    public String getTest(Member m) { // id=1&username=ssar&password=1234&email=ssar@nate.com // MessageConverter(스프링)
+        return "get 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    // TODO: 2) post 요청 시  RequestBody 를 사용하여 파싱한다.
+    // TODO: http://localhost:8080/http/post(insert)
+    @PostMapping("/http/post")
+    // TODO: (2-1) text/plain 으로 보내는 방법
+//    public String postTest(@RequestBody String test) { // MessageConverter(스프링)
+//        return "post 요청 : " + test ;
+//    }
+    // TODO: (2-2) application/json 으로 보내는 방법
+    public String postTest(@RequestBody Member m) { // MessageConverter(스프링)
+        return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    // TODO: http://localhost:8080/http/put(update)
+    @PutMapping("/http/put")
+    public String putTest(@RequestBody Member m) {
+        return "put 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    // TODO: http://localhost:8080/http/get(delete)
+    @DeleteMapping("/http/delete")
+    public String deleteTest() {
+        return "delete 요청";
+    }
+
+} // end of class
---
+++ backend-springboot/ToyBlog/src/main/java/com/example/toyblog/test/Member.java
@@ -0,0 +1,80 @@
+package com.example.toyblog.test;
+
+import lombok.*;
+
+/**
+ * ======================================
+ * FileName : Member
+ * Author : DH.Lee
+ * Date : 2023-12-12
+ * Note : http 요청 실습 모델 클래스
+ * 1) 필드 정의 : 클래스의 구성 요소
+ * 2) 생성자 정의 : 객체의 필드 초기화
+ * 3) getter, setter 메소드 정의 : 게터 메소드는 각 필드의 값을 반환하고, 세터 메소드는 각 필드에 새로운 값을 설정
+ * 4) Lombok 라이브러리 사용 : 복잡한 생성자를 수동으로 작성하는 수고를 덜고 코드의 가독성을 높일 수 있다.
+ * @Data : 자바 클래스에 대해 자주 사용되는 메소드들을 자동으로 생성해주는 역할로 아래 생략 어노ㅔ이션을 자동 생략할 수 있게 자동 제공하여 가독성을 높임
+ * @AllArgsConstructor : 클래스의 모든 필드를 매개변수로 갖는 전체 생성자(Full-Argument Constructor)를 자동으로 생성
+ * @NoArgsConstructor : 매개변수가 없는 기본 생성자(Default Constructor)를 자동으로 생성
+ * @Builder : 객체를 단계적으로 생성할 수 있는 빌더 패턴을 간단하게 구현하는 어노테이션
+ * ======================================
+ */
+// TODO: Lombok 라이브러리
+@Data // TODO: 자바 클래스에 대해 자주 사용되는 메소드들을 자동으로 생성해주는 역할로 아래 생략 어노테이션을 자동 제공하여 가독성을 높임
+// 생략가능해짐 @Getter, @Setter
+// 생략가능해짐 @toString(), @equals(), @hashCode()
+@AllArgsConstructor // TODO: 클래스의 모든 필드를 매개변수로 갖는 전체 생성자(Full-Argument Constructor)를 자동으로 생성
+@Builder // TODO:
+@NoArgsConstructor  // TODO: 매개변수가 없는 기본 생성자(Default Constructor)를 자동으로 생성
+
+public class Member {
+    // TODO: 1) 필드 정의
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    // TODO: 2) 생성자 정의
+//    @Builder // TODO: 빌더 패턴은 객체 생성을 위한 디자인 패턴으로
+             //      객체의 생성 과정과 표현 방법을 분리하여, 동일한 생성 과정에서 다른 표현 결과를 얻을 수 있게 합니다.
+             //      이 패턴은 특히 많은 매개변수를 갖는 객체를 생성할 때 유용
+//    public Member(int id, String username, String password, String email) {
+//        this.id = id;
+//        this.username = username;
+//        this.password = password;
+//        this.email = email;
+//    }
+//
+//    // TODO: 3) getter, setter 메소드 정의
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+
+} // end of class
